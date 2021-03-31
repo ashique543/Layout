51ac455bc249c6c074b824f2f557571babcfb149
Your Name <ashiquemullick14@gmail.com>
2021-03-31 10:43:48 +0530

    php update

--- php/send-mail.php
+++ php/send-mail.php
@@ -9,14 +9,6 @@ if(isset($_POST['submit']))
     $subject='Form Submission';
     $message="Name: ".$name."\n"."E-Mail: ".$email."\n"."Wrote the Following: "."\n\n".$msg;
     $headers="From: ".$email;
-
-    if(mail($to,$subject,$message,$headers))
-    {
-        echo "<h2> Sent Successfully! Thank You"." ".$name.", We will contact you</h2>";
-    }
-    else
-    {
-        echo "<h2>Something Went Wrong</h2>";
-    }
+    mail($to,$subject,$message,$headers);
 }
 ?>
